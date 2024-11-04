5d788be442e7edb7a87130962cd7b3cbcc8fc505
DlHugoo <hugo.davelawrence@gmail.com>
2024-11-04 18:53:58 +0800

    Routing

--- swift/src/App.js
+++ swift/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Test from './pages/test/Test';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
   );
 }
 
--- swift/src/pages/home/Home.jsx
+++ swift/src/pages/home/Home.jsx
@@ -1,13 +1,24 @@
+import { Button } from '@mui/material';
 import ResponsiveAppBar from '../../component/Appbar';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import AvatarTheme from '../../component/Theme';
 import Body from './Body';
 
 function Home() {
+    const navigate = useNavigate();
+    
   return (
     <div className="App">
       <ResponsiveAppBar/>
       <AvatarTheme/>
       <Body/>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={() => navigate('/test')} // Redirect to /test on click
+      >
+        Go to Test Page
+      </Button>
     </div>
   );
 }
