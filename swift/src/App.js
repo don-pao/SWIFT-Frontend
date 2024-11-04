83159ab3b5d8cace19e202f90ff643dc908a8111
BlackLazuli <broxephos@gmail.com>
2024-11-04 15:29:33 +0800

    Put the routes to App.js

--- swift/src/App.js
+++ swift/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './homePage/Home';
+import FlashcardForm from './homePage/Flashcard';
+import QuizForm from './homePage/Quiz';
+import FlashcardSetForm from './homePage/FlashcardSet';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/flashcard-set-form" element={<FlashcardSetForm />} />
+          <Route path="/flashcard-form/:setId" element={<FlashcardForm />} />
+          <Route path="/quiz-form/:setId" element={<QuizForm />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- swift/src/homePage/Home.jsx
+++ swift/src/homePage/Home.jsx
@@ -1,24 +1,13 @@
 import ResponsiveAppBar from './Appbar';
 import Body from './Body';
 import AvatarTheme from './Theme';
-import FlashcardForm from './Flashcard';
-import QuizForm from './Quiz';
-import FlashcardSetForm from './FlashcardSet';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="App">
-      <ResponsiveAppBar/>
-      <AvatarTheme/>
-      <Router>
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/flashcard-set-form" element={<FlashcardSetForm />} />
-        <Route path="/flashcard-form/:setId" element={<FlashcardForm />} />
-        <Route path="/quiz-form/:setId" element={<QuizForm />} />
-      </Routes>
-    </Router>
+      <ResponsiveAppBar />
+      <AvatarTheme />
+      <Body />
     </div>
   );
 }
