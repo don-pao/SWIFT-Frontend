3a0674de75a2b3138616f581de17299654bc4325
DlHugoo <hugo.davelawrence@gmail.com>
2024-11-04 18:38:23 +0800

    Redo the boilerplate code

---
+++ swift/src/component/Theme.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AvatarTheme() {
+  return (
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+      <img
+        src='./images/themes/theme.png'
+        alt="Wide view"
+        style={{ width: '100%', maxHeight: '500px', objectFit: 'cover' }} // Adjust the height as needed
+      />
+    </div>
+  );
+}
+
+export default AvatarTheme;
---
+++ swift/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import ResponsiveAppBar from '../../component/Appbar';
+import AvatarTheme from '../../component/Theme';
+import Body from './Body';
+
+function Home() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar/>
+      <AvatarTheme/>
+      <Body/>
+    </div>
+  );
+}
+
+export default Home;
