a59a2b439b4a21dadcdf2875a1dc19bcdb0549e2
KDazai <feerdeellaban556@gmail.com>
2024-11-05 09:22:46 +0800

    Path Shop

--- swift/src/App.js
+++ swift/src/App.js
@@ -5,6 +5,7 @@ import Test from './pages/test/Test';
 import FlashcardForm from './pages/flashcard/Flashcard';
 import QuizForm from './pages/quiz/Quiz';
 import FlashcardSetForm from './pages/flashcardset/FlashcardSet';
+import Shop from './pages/shop/Shop';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/flashcard-set-form" element={<FlashcardSetForm />} />
           <Route path="/flashcard-form/:setId" element={<FlashcardForm />} />
           <Route path="/quiz-form/:setId" element={<QuizForm />} />
+          <Route path="/shop" element={<Shop />} />
       </Routes>
     </Router>
   );
